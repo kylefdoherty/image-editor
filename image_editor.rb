e389040c2ca9bad0636f893da8fa414d6da47325
Kyle Doherty <kyledoherty22@gmail.com>
2014-02-14 17:44:07 +0100

    image_edit resizes files to the correct width

--- image_editor.rb
+++ image_editor.rb
@@ -1,11 +1,21 @@
 require 'fileutils'
+require 'rmagick'
+
 class ImageEditor
 	def initialize(options)
 		@options = options
 	end
 	def run
-		@options.input_files
 		FileUtils.mkdir(@options.output)
-		FileUtils.cp(@options.input_files, @options.output)
+		resize 
 	end
+
+	def resize 
+		@options.input_files.each do |file|
+			img = Magick::Image::read(file)[0]
+			img.resize_to_fit!(1000)
+			output_path = File.join(@options.output, File.basename(file))
+			img.write(output_path)
+		end 
+	end 
 end
--- spec/acceptance/resizing_two_images_to_width_1000_spec.rb
+++ spec/acceptance/resizing_two_images_to_width_1000_spec.rb
@@ -1,20 +1,37 @@
 require 'fileutils'
+require 'rmagick'
 
 describe 'ruby image_edit.rb' do 
 	describe '--width 1000 --output  sandbox img1.jpg img2.jpg' do 
-		it 'should save img1.jpg in sandbox' do
-			# 1. build up state / get system into know state
+		before(:each) do
 			if File.exists?("sandbox")
 				FileUtils.rm_rf("sandbox")
 			end
-			FileUtils.touch(["img1.jpg", "img2.jpg"])
+		end
+		it 'should save img1.jpg in sandbox' do
+			# 1. build up state / get system into know state
+			
 			# 2. execute program
-			output = `ruby image_edit.rb --width 1000 --output sandbox img1.jpg img2.jpg`
+			output = `ruby image_edit.rb --width 1000 --output sandbox spec/fixtures/img1.jpg spec/fixtures/img2.jpg`
 
 			# 3. assert against state or output
 			File.exists?("sandbox/img1.jpg").should == true
+			File.exists?("sandbox/img2.jpg").should == true
 		end
-		# it 'should resize img1.jpg to 1000px width'
+		
+		it 'should resize img1.jpg to 1000px width' do 
+			# 1. build up state
+
+			# 2. execute program 
+			output = `ruby image_edit.rb --width 1000 --output sandbox spec/fixtures/img1.jpg spec/fixtures/img2.jpg`
+
+			# 3. assert against state or output
+			img = Magick::Image::read('sandbox/img1.jpg').first
+			img.columns.should == 1000
+			img = Magick::Image::read('sandbox/img2.jpg').first
+			img.columns.should == 1000
+
+		end 
 		# it 'should resize img1.jpg to 500px height'
 	end 
 	
